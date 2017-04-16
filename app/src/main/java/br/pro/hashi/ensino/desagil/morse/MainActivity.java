8cf7dfbb5af4a656f2865ec8c5d41fe442c9241d
Raphael costa <Raphael costa>
2017-04-16 17:31:05 -0300

    Atualização design

--- app/src/main/java/br/pro/hashi/ensino/desagil/morse/MainActivity.java
+++ app/src/main/java/br/pro/hashi/ensino/desagil/morse/MainActivity.java
@@ -2,24 +2,12 @@ package br.pro.hashi.ensino.desagil.morse;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    private Button tapButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        tapButton = (Button) findViewById(R.id.myButton);
-        tapButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v){
-
-            }
-        });
-
     }
 }
