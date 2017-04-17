e09ca3663c2d7073132742c3b1cf8e4b72fefde8
Elisa Malzoni <elisamm@al.insper.edu.br>
2017-04-17 11:34:19 -0300

    lista de mensagens, atulizada

--- app/src/main/java/br/pro/hashi/ensino/desagil/morse/MsgList.java
+++ app/src/main/java/br/pro/hashi/ensino/desagil/morse/MsgList.java
@@ -1,6 +1,7 @@
 package br.pro.hashi.ensino.desagil.morse;
 
 import java.util.LinkedList;
+import java.util.List;
 
 /**
  * Created by elisa on 17/04/2017.
@@ -20,7 +21,7 @@ public class MsgList {
 
     }
 
-    public getMessages(){
+    public List<String> getMessages(){
         return messages;
     }
 }
