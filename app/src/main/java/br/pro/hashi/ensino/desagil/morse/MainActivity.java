50133cfc3a3d75887206fc6e0c8b34cdf5488ebf
guizg <guizgraicer@gmail.com>
2017-04-18 12:48:59 -0300

    enviando mensagens prontas

--- app/src/main/java/br/pro/hashi/ensino/desagil/morse/MainActivity.java
+++ app/src/main/java/br/pro/hashi/ensino/desagil/morse/MainActivity.java
@@ -6,20 +6,24 @@ import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.telephony.SmsManager;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.Toast;
 
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
     private static final int REQUEST_EXAMPLE = 0;
 
     private List<String> mensagens;
     private ArrayAdapter<String> listAdapter;
     private Spinner dropdown;
     private MsgList listaDeMsgs;
+    private String msgtosend;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,13 +31,31 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         listaDeMsgs = new MsgList();
         mensagens = listaDeMsgs.getMessages();
-
+        msgtosend = null;
         listAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, mensagens);
 
         dropdown = (Spinner)findViewById(R.id.spinner);
+        dropdown.setOnItemSelectedListener(this);
 
         dropdown.setAdapter(listAdapter);
 
+    }
+
+    public void sendMessage(View view) {
+        SmsManager manager = SmsManager.getDefault();
+
+        String number = "011999331669"; //colocar o numero do cuidador
+        String message = msgtosend;
+
+        try {
+            manager.sendTextMessage(number, null, message, null, null);
+
+            Toast toast = Toast.makeText(this, "ENVIADO!", Toast.LENGTH_SHORT);
+            toast.show();
+        }
+        catch(IllegalArgumentException exception){
+            Log.e("SendActivity", "number or message empty");
+        }
     }
 
 
@@ -42,7 +64,8 @@ public class MainActivity extends AppCompatActivity {
         if(permission == PackageManager.PERMISSION_GRANTED) {
             Toast toast = Toast.makeText(this, "Enviando SMS!", Toast.LENGTH_SHORT);
             toast.show();
-            /// Aqui vão as chamadas das funções para propriamente enviar as mensagens para um número a ser definido.
+            /// Aqui vão as chamadas das funções para propriamente enviar as mensagens para um número a ser definido
+            sendMessage(view);
         }
         else {
             String[] permissions = new String[1];
@@ -64,6 +87,17 @@ public class MainActivity extends AppCompatActivity {
                 toast.show();
             }
         }
+    }
+
+    public void onItemSelected(AdapterView<?> parent, View view,
+                               int pos, long id) {
+        // An item was selected. You can retrieve the selected item using
+        // parent.getItemAtPosition(pos)
+        msgtosend = parent.getItemAtPosition(pos).toString();
+    }
+
+    public void onNothingSelected(AdapterView<?> parent) {
+        // Another interface callback
     }
 
 }
