00e459f47796942834204b9d8ff14f4c923af40c
guizg <guizgraicer@gmail.com>
2017-05-02 00:21:34 -0300

    -

---
+++ app/src/main/java/br/pro/hashi/ensino/desagil/morse/RomanToMorse.java
@@ -0,0 +1,26 @@
+package br.pro.hashi.ensino.desagil.morse;
+
+import java.util.HashMap;
+
+/**
+ * Created by elisa on 26/04/2017.
+ */
+
+public class RomanToMorse {
+    public String letterList;
+    private MorseTree tree = new MorseTree();
+    private HashMap<Character, String> dic;
+
+    public RomanToMorse(){
+        letterList = "abcdefghijklmnopqrstuvwxyz123456789+=/";
+        dic = new HashMap<>();
+        for (int i=0; i < letterList.length(); i++) {
+            String morse = tree.codigo(letterList.charAt(i));
+            dic.put(letterList.charAt(i), morse);
+        }
+    }
+
+    public HashMap<Character, String> getResult(){
+        return dic;
+    }
+}
