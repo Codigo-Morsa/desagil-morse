fff15f2825fd7dca9b78570a913173b35419d5a3
guizg <guizgraicer@gmail.com>
2017-05-02 21:32:48 -0300

    fredao da massa

--- app/src/main/java/br/pro/hashi/ensino/desagil/morse/activity_dicromantomorse.java
+++ app/src/main/java/br/pro/hashi/ensino/desagil/morse/activity_dicromantomorse.java
@@ -12,12 +12,19 @@ import android.widget.GridLayout;
 public class activity_dicromantomorse extends AppCompatActivity {
     private String[] listaRoman;
     private String[] listaMorse;
+    private RomanToMorse rtm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dicromantomorse);
-
+        String letterList = "abcdefghijklmnopqrstuvwxyz1234567890+=/";
+        listaRoman = new String[letterList.length()];
+        for (int i=0; i < letterList.length(); i++) {
+            listaRoman[i]=Character.toString(letterList.charAt(i));
+        }
+        rtm = new RomanToMorse();
+        
 
 
     }
